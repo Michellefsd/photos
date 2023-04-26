fb74949ba621bb4f0701e78e3ea57b6d0ade6457
Michellefsd <promichfsd@gmail.com>
2023-04-26 18:32:51 -0300

    adding the user api with thunks

--- src/store/apis/albumApi.js
+++ src/store/apis/albumApi.js
@@ -4,6 +4,34 @@ const albumsApi = createApi({
     reducerPath: "albums",
     fetchBaseQuery: fetchBaseQuery({
         baseUrl: "http://localhost:3005",
-    })
+    }),
+    endpoints(builder) {
+        return {
+            fetchAlbums: builder.query({
+                query: (user) => {
+                    return {
+                        url: "./albums",
+                        params: {
+                            userId: user.id,
+                        },
+                        method: 'GET',
+                    };
+                }
+            }),
+            // createAlbum: builder.mutation({
+            //     query: (userId) => {
+            //         return {
+            //             url: "./albums",
+            //             method: 'POST',
+            //             body: {title, userId}
+            //         };
+            //     }
+            // }),
+        };
+    }
 });
 
+albumsApi.useFetchAlbumsQuery();
+
+export const { useFetchAlbumsQuery } = albumsApi;
+export { albumsApi };
--- src/store/apis/dist/albumApi.dev.js
+++ src/store/apis/dist/albumApi.dev.js
@@ -1,11 +1,44 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.albumsApi = exports.useFetchAlbumsQuery = void 0;
+
 var _react = require("@reduxjs/toolkit/query/react");
 
 var albumsApi = (0, _react.createApi)({
   reducerPath: "albums",
   fetchBaseQuery: (0, _react.fetchBaseQuery)({
     baseUrl: "http://localhost:3005"
-  })
+  }),
+  endpoints: function endpoints(builder) {
+    return {
+      fetchAlbums: builder.query({
+        query: function query(user) {
+          return {
+            url: "./albums",
+            params: {
+              userId: user.id
+            },
+            method: 'GET'
+          };
+        }
+      }) // createAlbum: builder.mutation({
+      //     query: (userId) => {
+      //         return {
+      //             url: "./albums",
+      //             method: 'POST',
+      //             body: {title, userId}
+      //         };
+      //     }
+      // }),
+
+    };
+  }
 });
+exports.albumsApi = albumsApi;
+albumsApi.useFetchAlbumsQuery();
+var useFetchAlbumsQuery = albumsApi.useFetchAlbumsQuery;
+exports.useFetchAlbumsQuery = useFetchAlbumsQuery;
 //# sourceMappingURL=albumApi.dev.js.map
